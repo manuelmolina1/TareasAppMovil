a75a4ae33460c9f1cf260eb383e268cfc398f6cc
DESKTOP-MEB474V\Usuario <manymolina06@gmail.com>
2022-03-17 21:46:36 -0600

    Tarea Ionic

---
+++ src/app/pages/residencia/residencia.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-residencia',
+  templateUrl: './residencia.page.html',
+  styleUrls: ['./residencia.page.scss'],
+})
+export class ResidenciaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
